f7948ffaf0de64c391cbb4b80f18d7f247d1329d
Dmitrii Esin <dmitrii_esin@epam.com>
2021-06-18 15:03:57 +0300

    cart-api service added, cart-api dockerized

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://nr3xtj1l4g.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  //TODO: Fix to deployaed app
+  cart: "http://127.0.0.1",
 };
 
 export default API_PATHS;
