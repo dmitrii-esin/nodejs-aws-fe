88f88e4865fbde5b3eecb6dca94fc8a605dce84e
Dmitrii Esin <dmitrii_esin@epam.com>
2021-06-20 19:35:33 +0300

    refactor

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,6 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://nr3xtj1l4g.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  //TODO: Fix to deployed app
   cart: "https://dmitrii-esin-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
 };
 
