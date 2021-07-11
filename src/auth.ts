0e0244676e55385fbf6c2920bbdd0dc6addb6b1e
Dmitrii Esin <dmitrii_esin@epam.com>
2021-07-11 13:47:27 +0300

    work with bff started, WIP

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,10 @@
 import { getTokenFromHref } from "utils/utils";
 
+//TODO:!!!! remove comments !!!!
 export const checkAuth = () => {
-  const loginPageUrl = `${process.env.REACT_APP_COGNITO_LOGIN_PAGE_DOMAIN}/login?client_id=${process.env.REACT_APP_COGNITO_CLIENT_ID}&response_type=${process.env.REACT_APP_COGNITO_RESPONSE_TYPE}&scope=${process.env.REACT_APP_COGNITO_SCOPE}&redirect_uri=${process.env.REACT_APP_URL}`;
-
-  const token = getTokenFromHref();
-
-  if (!token) {
-    window.location.replace(loginPageUrl);
-  }
+  // const loginPageUrl = `${process.env.REACT_APP_COGNITO_LOGIN_PAGE_DOMAIN}/login?client_id=${process.env.REACT_APP_COGNITO_CLIENT_ID}&response_type=${process.env.REACT_APP_COGNITO_RESPONSE_TYPE}&scope=${process.env.REACT_APP_COGNITO_SCOPE}&redirect_uri=${process.env.REACT_APP_URL}`;
+  // const token = getTokenFromHref();
+  // if (!token) {
+  //   window.location.replace(loginPageUrl);
+  // }
 };
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,13 @@
 const API_PATHS = {
-  product: "https://ie2svy46v9.execute-api.eu-west-1.amazonaws.com/dev",
+  //TODO: remove localhost to real deploued bff service
+  // product: "https://ie2svy46v9.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "http://localhost:3000/product",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://nr3xtj1l4g.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://dmitrii-esin-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
+  //TODO: remove localhost to real deploued bff srvice
+  // cart: "https://dmitrii-esin-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
+  cart: "http://localhost:3000/cart",
 };
 
 export default API_PATHS;
